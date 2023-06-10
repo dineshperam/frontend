4c1177f357647df53d7ca218787287be694ccdfa
dineshperam <pdreddy972@gmail.com>
2023-06-10 11:57:24 +0530

    updated frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ProductsSection from './components/Products';
-import RegistrationPage from './components/Registration';
-import LoginPage from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Products from './components/Products';
+import Registration from './components/Registration';
+import Login from './components/Login';
 import Navigation from './components/Navigation';
 import NotFound from './components/NotFound';
 import Home from './components/Home';
